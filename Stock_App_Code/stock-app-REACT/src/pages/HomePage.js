1276b30b361ab9dd94f2e6b4c9fde8f469d0d38f
MaggieHemond <134964391+MaggieHemond@users.noreply.github.com>
2025-03-28 13:06:35 -0400

    Layout for Home and Settings

--- Stock_App_Code/stock-app-REACT/src/pages/HomePage.js
+++ Stock_App_Code/stock-app-REACT/src/pages/HomePage.js
@@ -5,7 +5,20 @@ function HomePage() {
   return (
     <div>
       <h1>Home</h1>
-      <p>Welcome to the stock tracking app.</p>
+      <p>Welcome to the Stock Tracking App.</p>
+
+      <h2>Top five Stocks</h2>
+
+      <h2>Description of the Stock Tracking App</h2>
+      <p>
+      This is a website/app that is about adding stocks to your portfolio that give you a good price. 
+      There are four pages, this page where you see the top stocks and the description. The Stock page, 
+      where you can request to see stocks with their acronym, and then if they have good prices, you add them to your portfolio. 
+      The portfolio page is a page where you can look at all the stocks you have and delete any that aren’t performing well. 
+      The setting page is you can see and change something.
+      </p>
+
+
     </div>
   );
 }
--- Stock_App_Code/stock-app-REACT/src/pages/SettingsPage.js
+++ Stock_App_Code/stock-app-REACT/src/pages/SettingsPage.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Button } from "@mui/material";
+import '../stylesheets/HomePage.css';
 
 function SettingsPage() {
   return (
     <div>
       <h1>Settings</h1>
       <p>Customize your app experience.</p>
+
+      <h2>Light/Dark Mode</h2>
+      <Button></Button>
+
+      <h2>FAQ</h2>
+      <p></p>
     </div>
   );
 }
