8ce0a64b2ae1225d49f96a079314a7f2deb35996
MaggieHemond <134964391+MaggieHemond@users.noreply.github.com>
2025-04-14 14:15:20 -0400

    Font and dropdown

--- Stock_App_Code/stock-app-REACT/src/pages/SettingsPage.js
+++ Stock_App_Code/stock-app-REACT/src/pages/SettingsPage.js
@@ -5,30 +5,67 @@ import '../stylesheets/Light_Dark.css';
 
 //https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp
 // onload={changeClass}
+//https://www.w3schools.com/css/tryit.asp?filename=trycss_dropdown_text
 
 function SettingsPage() {
   
-  const changeClass = () => {
+  const changeMode = () => {
     var a = document.body;
     a.classList.toggle("dark");
   }
+
+  const changeFontSize = () => {
+    var a = document.body;
+    a.classList.toggle("large");
+  }
+
   
+
   return (
     <div> 
       <h1>Settings</h1>
       <p>Customize your app experience.</p>
   
       <h2>Light/Dark Mode</h2>
-      
-      <Button onClick={changeClass}
+
+      <div class="container">
+      <Button onClick={changeMode}
       variant="contained"
       color="primary"
       >Mode</Button>
+      </div>
+
+      <h2>Large/Small Font</h2>
+
+      <div class="container">
+      <Button onClick={changeFontSize}
+      variant="contained"
+      color="primary"
+      >Font</Button>
+      </div>
       
       <h2>FAQ</h2>
-      <p>If there is a problem loading a stock?</p>
+
+      <div class="container">
+
+      <div class="dropdown">
+      <span>If there is a problem loading a stock?</span>
+      <div class="dropdown-content">
       <p>There is a window where it resets so be patiant and try again in a three minutes.</p>
+      </div> 
+      </div>
+
+      <br></br><br></br>
+
+      <div class="dropdown">
+      <span>If there is a error on the site?</span>
+      <div class="dropdown-content">
+      <p>Please contact us about how you got that error.</p>
+      </div> 
+      </div>
       
+       </div>
+
     </div>
   );
 }
